e1d0505d72a6e90f155046fe3dd6891fb62c9422
tkd995 <tkd995@vols.utk.edu>
2025-01-28 13:24:48 -0500

    Update lib_info.cpp
    brief patch, format adjustment

--- lib_info.cpp
+++ lib_info.cpp
@@ -1,9 +1,10 @@
-//Name: Daniel Huynh
+//Name: Daniel Huynh and Ian Henson
 //Professor Scott Emrich
 //This code takes the file, parses the contents into their data structure, takes the '_' out, stores them into the struct
 #include <iostream>
 #include <map>
 #include <string>
+#include <unordered_map>
 #include <fstream>
 #include <sstream>
 using namespace std;
@@ -37,10 +38,13 @@ int main(int argc, char *argv[]){
 	int sec = 0;
 	int total = 0;
 	char hold;
+	
 	Song song;
 	Artist artist;
 	Album album;
 
+	unordered_map<string, Artist> Artsists;
+	
 	stringstream ss;
     while(getline(fin,line))
     {
@@ -50,45 +54,46 @@ int main(int argc, char *argv[]){
 		//title
         ss << line;
         ss >> temp;
-		song.title = temp;
-		//replaces '_' for white space
-		for(int i = 0; i < temp.length(); i++){
-			if(temp[i] == '_') temp[i] = ' ';
-		}
+	song.title = temp;
+	//replaces '_' for white space
+	for(int i = 0; i < temp.length(); i++){
+		if(temp[i] == '_') temp[i] = ' ';
+	}
 		//time
-		ss << line;
-		ss >> min >> hold >> sec;
-		total = (min * 60) + sec;
-		song.time = total;
-		//artist
-		ss << line;
-		ss >> temp;
-		//replaces '_' for white space
-		for(int i = 0; i < temp.length(); i++){
-			if(temp[i] == '_') temp[i] = ' ';
-		}
-		//Checks for dupe
-		if(artist.albums.find(temp) == artist.albums.end()){
-			artist.albums.insert(temp, song);
-		}
-		artist.name = temp;
+	ss << line;
+	ss >> min >> hold >> sec;
+	total = (min * 60) + sec;
+	song.time = total;
+	//artist
+	ss << line;
+	ss >> temp;
+	//replaces '_' for white space
+	for(int i = 0; i < temp.length(); i++){
+		if(temp[i] == '_') temp[i] = ' ';
+	}
+	//Checks for dupe
+	if(artist.albums.find(temp) == artist.albums.end()){
+		artist.albums.insert(temp, song);
+	}
+	artist.name = temp;
 
-		//album
+	//album
         ss << line;
         ss >> temp;
-		album.name = temp;
-		//replaces '_' for white space
-		for(int i = 0; i < temp.length(); i++){
-			if(temp[i] == '_') temp[i] = ' ';
-		}
-		//genre 'not used anywhere'
+	album.name = temp;
+	//replaces '_' for white space
+	for(int i = 0; i < temp.length(); i++){
+		if(temp[i] == '_') temp[i] = ' ';
+	}
+	//genre 'not used anywhere'
         ss << line;
         ss >> temp;
 		
 		//track
         ss << line;
         ss >> sec;
-		Album.songs.insert(sec, song);
+	Album.songs.insert(sec, song);
     }
-		fin.close();
+
+	fin.close();
 }
